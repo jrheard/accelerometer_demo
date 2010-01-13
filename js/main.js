f24bdad65335b6ab7c49dee798a7644e42f5984a
jrheard <jrheard@cs.stanford.edu>
2010-01-13 13:13:28 -0800

    most of the skeleton's coded up, just gotta figure out speed/position logic now

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
-var ball = new function() {
-	var speeds = {
-		x: 0,
-		y: 0,
-		z: 0
-	}
+function Ball(x, y, radius) {
+	this.radius = radius;
 
-	var position = {
+	this.position = {
 		x: 0,
 		y: 0,
 		z: 0
-	}
+	};
+
+	this.speeds = {
+		x: 0, // right-to-left tilt: 0 = level, 1 = way left, -1 = way right
+		y: 0, // front-to-back tilt: 0 = level, 1 = backward, -1 = forward
+		z: 0  // vertical acceleration: moving device upward makes this go down, 1 = standard earth gravity
+	};
+
 
 	this.setSpeeds = function(orientData) {
 		this.speeds = orientData;
 	};
 
 	this.move = function() {
+		for(dimension in this.position) {
+			//use this.speeds[dimension]
+		}
 
+		this.draw();
 	};
 
 	this.draw = function() {
+		var canvas = $("#canvas").get(0);
+		var ctx = canvas.getContext("2d");
 
+		ctx.beginPath();
+		ctx.fillStyle = "#000";
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, true);
+		ctx.closePath();
+		ctx.fill();
 	};
 };
 
-function handleOrientation(orientData) {
-	ball.setSpeeds(orientData.x, orientData.y, orientData.z);
-}
+$(function() {
+	var FRAMES_PER_SECOND = 30;
+	var BALL_RADIUS = 50;
 
-window.addEventListener("MozOrientation", handleOrientation, true);  
+	var width = $("body").width();
+
+	var canvas = $("#canvas").get(0);
+	canvas.setWidth(width);
+	canvas.setHeight(height);
+
+	var ball = new Ball(width/2, 0, BALL_RADIUS);
+
+
+	window.addEventListener("MozOrientation", ball.setSpeeds, true);  
+	setInterval(ball.move, 1000 / FRAMES_PER_SECOND);
+});
